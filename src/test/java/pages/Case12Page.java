98d4653967f3797c71c2289aded3b3f872147d51
Fatih Turker <fthturker80@gmail.com>
2022-11-14 12:13:48 +0300

    14.11.2022

--- src/test/java/pages/Case12Page.java
+++ src/test/java/pages/Case12Page.java
@@ -12,4 +12,18 @@ public class Case12Page {
 
     @FindBy(xpath = "//a[@href='/products']")
     public WebElement productsButton;
+
+    @FindBy(xpath = "//a[@class='btn btn-default add-to-cart']")
+    public WebElement firstAddToCartButton;
+
+    @FindBy(xpath = "//button[@class='btn btn-success close-modal btn-block']")
+    public WebElement ContinueShoppingButton;
+
+    @FindBy(xpath = "(//a[contains(text(),'Add to cart')])[3]")
+    public WebElement secondAddTocartButton;
+
+    @FindBy(xpath = "//p[@class='text-center']//a")
+    public WebElement viewCartButton;
+
+
 }
--- src/test/java/stepDefinitions/AddProductsInCartStepDefinitions.java
+++ src/test/java/stepDefinitions/AddProductsInCartStepDefinitions.java
@@ -12,34 +12,45 @@ public class AddProductsInCartStepDefinitions {
 
     @When("Click Products button")
     public void click_products_button() {
-        Driver.waitAndClick(case12Page.productsButton,1);
+        Driver.waitAndClick(case12Page.productsButton, 1);
         Driver.wait(1);
         Log.info("Urunler dugmesine tiklandi");
     }
+
     @And("Hover over first product and click Add to cart")
     public void hoverOverFirstProductAndClickAddToCart() {
-
+        Driver.waitAndClick(case12Page.firstAddToCartButton, 1);
+        Driver.wait(1);
+        Log.info("Ilk urunun uzerine gelindi ve sepete ekle tiklandi");
     }
+
     @When("Click Continue Shopping button")
     public void click_continue_shopping_button() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        Driver.waitAndClick(case12Page.ContinueShoppingButton, 1);
+        Driver.wait(1);
+        Log.info("Alisverise Devam Et dugmesine tiklandi");
     }
+
     @When("Hover over second product and click Add to cart")
     public void hover_over_second_product_and_click_add_to_cart() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        Driver.waitAndClick(case12Page.secondAddTocartButton, 1);
+        Driver.wait(1);
+        Log.info("Fareyi ikinci urunun uzerine getirin ve Sepete ekle yi tiklayin");
     }
+
     @When("Click View Cart button")
     public void click_view_cart_button() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        Driver.waitAndClick(case12Page.viewCartButton, 1);
+        Driver.wait(1);
+        Log.info("Sepeti Goruntule dugmesine tiklandi");
     }
+
     @When("Verify both products are added to Cart")
     public void verify_both_products_are_added_to_cart() {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
+
     @When("Verify their prices, quantity and total price")
     public void verify_their_prices_quantity_and_total_price() {
         // Write code here that turns the phrase above into concrete actions
